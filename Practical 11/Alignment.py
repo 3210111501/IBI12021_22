92dd87a68c4d29e77513fd76afe3c1afaa6244a8
unknown <jingyiz.21@intl.zju.edu.cn>
2022-05-25 02:56:05 +0800

    new version of Practical11

--- Practical 11/Alignment.py
+++ Practical 11/Alignment.py
@@ -3,23 +3,21 @@ edit_distance=0
 def alignment(seq1,seq2):
     global edit_distance
     for i in range(len(seq1)):
-        if seq1[i]!=seq2[i]:
+        if seq1[i]==seq2[i]:
            edit_distance+=1
-#set alignment score equals to edit_distance
-    print("alignment score=",edit_distance)
-
 
 #define a function that can calculate the percentage of indentical amino acides for each comparison
-def percentage(x,y):
+def percentage(x,y,z):
     if len(x)>=len(y):
         p=x
     else:
         p=y
     alignment(x,y)
     percentage=edit_distance/len(p)
-    print(percentage)
+    print('The percentage of identical amino acids between',z,'is',percentage)
 
 #define a function which can read fasta document
+#The instruction from https://blog.csdn.net/qq_18369669/article/details/103408600 is used for the reference of the code.
 def readFa(fa):
     with open(fa, 'r') as FA:
         seqName, seq = '', ''
@@ -36,25 +34,85 @@ def readFa(fa):
             if not line: break
 
 #read three documents and get three sequences
-fa = "C:/Users/zuojingyi/GIT/IBI1_2021-22/Practical10/DLX5_human.fa"
-for (seqName,seq) in readFa(fa):
+fa = "D:/GIT/IBI1_2021-22/Practical 11/DLX5_human.fa"
+for (sqName,seq) in readFa(fa):
     a=seq
 print(a)
-fa = "C:/Users/zuojingyi/GIT/IBI1_2021-22/Practical10/DLX5_mouse.fa"
+fa = "D:/GIT/IBI1_2021-22/Practical 11/DLX5_mouse.fa"
 for (seqName,seq) in readFa(fa):
     b=seq
 print(b)
-fa = "C:/Users/zuojingyi/GIT/IBI1_2021-22/Practical10/RandomSeq(1).fa"
+fa = "D:/GIT/IBI1_2021-22/Practical 11/RandomSeq(1).fa"
 for (seqName,seq) in readFa(fa):
     c=seq
 print(c)
 
 #every time you output a result, make the global variable into 0
-percentage(a,b)
+percentage(a,b,'human and mouse')
+
 edit_distance=0
-percentage(b,c)
+percentage(a,c,'human and random')
+
 edit_distance=0
-percentage(a,c)
+percentage(b,c,'mouse and random')
+#The following part is finished under the instruction of Jianzhang Lu.
+#import BLOSUM62
+amino_acid_list=['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V','B','Z','X','*']
+BLOSUM62 = [
+            [ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0, -4],
+            [-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1, -4],
+            [-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1, -4],
+            [-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+            [ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2, -4],
+            [-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1, -4],
+            [-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+            [ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1, -4],
+            [-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1, -4],
+            [-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1, -4],
+            [-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1, -4],
+            [-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1, -4],
+            [-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1, -4],
+            [-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1, -4],
+            [-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2, -4],
+            [ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0, -4],
+            [ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0, -4],
+            [-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2, -4],
+            [-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1, -4],
+            [ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1, -4],
+            [-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+            [-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+            [ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1, -4],
+            [-4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4,  1],
+]
+#define two functions to align two amino acid sequences and acquire the total score
+def index_sequence(w):
+    index=amino_acid_list.index(w)
+    return index
+def score(x,y,z):
+    total_score=0
+    if len(x)>=len(y):
+        p=x
+    else:
+        p=y
+    alignment(x,y)
+    for item in range (len(p)):
+        w1 = x[item]
+        index1=index_sequence(w1)
+        w2 = y[item]
+        index2=index_sequence(w2)
+        score=BLOSUM62[index1][index2]
+        total_score+=score
+    print('The score between',z,'is',total_score)
+#conduct the functions
+score(a,b,'human and mouse')
+score(a,c,'human and random')
+score(b,c,'mouse and random')
+
+
+
+
+
+
 
 
 
